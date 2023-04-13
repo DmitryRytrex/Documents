200b035e3aacfed2d35b3ac6b216ba8c6d56bc94
student <student@1414-05>
2023-04-13 19:00:47 +0300

    Добавьте файлы проекта.

---
+++ Documents/FormAuth.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Documents.DBConnect;
+
+namespace Documents
+{
+    public partial class FormAuth : Form
+    {
+        public FormAuth()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+
+
+            string login = textBox1.Text;
+            string password = textBox2.Text;
+
+            using (DataBase db = new DataBase())
+            {
+                DataTable users = db.ExecuteSql($"select * from [USER] where [Login] = '{login}' and [Password] = '{password}'");
+
+                if (users.Rows.Count > 0) //||userLevel)
+                {
+                    FormMain Main = new FormMain();
+                    Main.Show();
+                    Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный логин или пароль! Проверьте корректность введенных данных.");
+                    count++;
+                }
+                if (count >= 3)
+                {
+                    const string message = "Неправильный логин или пароль. Хотите восстановить доступ к аккаунту?";
+                    const string caption = "Восстановление доступа";
+                    var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+
+                    if (result == DialogResult.Yes)
+                    {
+                        FormRecover recover = new FormRecover();
+                        recover.Show();
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Documents/FormRecover.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Documents.DBConnect;
+
+namespace Documents
+{
+    public partial class FormRecover : Form
+    {
+        public FormRecover()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.PasswordChar = '*';
+            textBox3.PasswordChar = '*';
+            string login = textBox1.Text;
+            string password = textBox2.Text;
+            string confirm = textBox3.Text;
+            string symb = "ABCDEFGHIJKLMNOPRSTUVWXYZ123456789!\"#$%&'()*+,-./::<=>?@[\\]:_{|}";
+            if (textBox2.Text.IndexOfAny(symb.ToCharArray()) == -1 || textBox2.Text.Length < 8 || textBox2.Text != textBox3.Text)
+            {
+                MessageBox.Show("Пароль не соответствует стандартам. Используйте в пароле как минимум 5 букв и 3 цифры, а также знаки @#%");
+            }
+            else
+            {
+                using (DataBase db = new DataBase())
+                {
+
+                    db.ExecuteSqlNonQuery($"UPDATE USER WHERE [LOGIN] = '{textBox1.Text}' SET [PASSWORD] = {textBox2.Text}");
+
+                }
+            }
+        }
+    }
+}
